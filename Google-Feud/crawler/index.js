ce9461422d9c17ddfe9371693da0476edc76fe14
paulovelho <paulovelho@paulovelho.com>
2015-05-11 16:18:37 +0100

    google feud crawler

---
+++ Google-Feud/crawler/index.js
@@ -0,0 +1,36 @@
+var log = require('color-logs')(true, true, __filename);
+var fs = require("fs");
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var server = app.listen(8007, function(){
+
+	var template_options = {
+		root: __dirname + '/templates/',
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	};
+	var host = server.address().address;
+	var port = server.address().port;
+
+	log.Debug("app listening at  http://%s:%s", host, port);
+	
+	app.use("/javascript", express.static("javascript"));
+	app.get("/", function(req, res){
+		res.sendFile("index.html", template_options, function(err){
+			if(err) log.Error(err);
+		});
+	});
+
+	app.post("/feud", function(req, res){
+		var query = req.body.query;
+		res.send("looking for " + query);
+	});
+
+});
